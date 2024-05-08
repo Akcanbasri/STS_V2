e7bb92f56a0bab875f2cd1880958d5e14aec6b0e
Hasan Basri Akcan <akcanhasanbasri@hotmail.com>
2024-05-08 12:11:36 +0300

    feat: Add CORS support for localhost:3000

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
     "api.apps.ApiConfig",
+    "corsheaders",
 ]
 
 REST_FRAMEWORK = {
@@ -73,11 +74,16 @@ SIMPLE_JWT = {
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 AUTH_USER_MODEL = "api.User"
